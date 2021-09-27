c16345b6100b74a572a581bd004a6dfa9f2b2123
Mi-racle <872137422@qq.com>
2021-09-27 15:51:35 +0800

    change name

--- src/main/java/cn/edu/sspu/miracle/movingball/MovingBallApplication.java
+++ src/main/java/cn/edu/sspu/miracle/movingball/MovingBallApplication.java
@@ -7,9 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.*;
 import javafx.scene.shape.*;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -20,33 +18,44 @@ import java.util.Objects;
 public class MovingBallApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MovingBallApplication.class.getResource("moving-ball.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 960, 640);
-        stage.setTitle("Look at the ball!");
-        AnchorPane base = (AnchorPane) scene.lookup("#base");
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                MovingBallApplication.class.getResource("moving-ball.fxml")
+        );
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        stage.setTitle("Look at the car!");
+        VBox vbox = (VBox) scene.lookup("#vbox");
+        AnchorPane anchorPane = (AnchorPane) scene.lookup("#base");
+        BackgroundImage backgroundImage = new BackgroundImage(
+                new Image(Objects.requireNonNull(
+                        MovingBallApplication.class.getResource("focus/BGROUND.BMP")
+                ).toString()),
+                BackgroundRepeat.REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT
+        );
+        Background background = new Background(backgroundImage);
+        vbox.setBackground(background);
 
-        // Circle circle = (Circle) scene.lookup("#circle");
         Image image = new Image(
                 Objects.requireNonNull(
-                        MovingBallApplication.class.getResource("FOCUS.BMP")
+                        MovingBallApplication.class.getResource("focus/FOCUS.BMP")
                 ).toString()
         );
         ImageView imageView = new ImageView(image);
-        base.getChildren().add(imageView);
+        anchorPane.getChildren().add(imageView);
 
         Path path=new Path();
-        path.getElements().add(new MoveTo(0, 320));
-        path.getElements().add(new LineTo(840, 320));
-        path.getElements().add(new MoveTo(840, 320));
-        path.getElements().add(new LineTo(0, 320));
+        path.getElements().add(new MoveTo(0, 360));
+        path.getElements().add(new LineTo(1280, 360));
+        //path.getElements().add(new MoveTo(1280, 360));
+        path.getElements().add(new LineTo(0, 360));
 
         PathTransition pt=new PathTransition();
         pt.setDuration(Duration.millis(4000));//设置持续时间4秒
         pt.setPath(path);//设置路径
         pt.setNode(imageView);//设置物体
-        pt.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
-        //设置周期性，无线循环
-        pt.setCycleCount(Timeline.INDEFINITE);
+        pt.setCycleCount(Timeline.INDEFINITE);//设置周期性，无线循环
         pt.setAutoReverse(true);
         pt.play();
 

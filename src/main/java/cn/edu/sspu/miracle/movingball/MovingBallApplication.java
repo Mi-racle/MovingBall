e70dd6d12c5b14d4e1660e38a1aad5a0d21808e1
Mi-ralce <872137422@qq.com>
2021-09-29 01:07:48 +0800

    adjust

--- src/main/java/cn/edu/sspu/miracle/movingball/MovingBallApplication.java
+++ src/main/java/cn/edu/sspu/miracle/movingball/MovingBallApplication.java
@@ -1,23 +1,24 @@
 package cn.edu.sspu.miracle.movingball;
 
-import javafx.animation.PathTransition;
+import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
-import javafx.scene.shape.*;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 import javafx.util.Duration;
 
 import java.io.IOException;
 import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MovingBallApplication extends Application {
     @Override
@@ -26,7 +27,7 @@ public class MovingBallApplication extends Application {
                 MovingBallApplication.class.getResource("moving-ball.fxml")
         );
         Scene scene = new Scene(fxmlLoader.load(), 1024, 768);
-        stage.setTitle("Look at the car!");
+        stage.setTitle("Look at the ball!");
         VBox vbox = (VBox) scene.lookup("#vbox");
         //AnchorPane anchorPane = (AnchorPane) scene.lookup("#base");
         Canvas canvasTop = (Canvas) scene.lookup("#canvasTop");
@@ -59,15 +60,16 @@ public class MovingBallApplication extends Application {
         GraphicsContext graphicsContextTop = canvasTop.getGraphicsContext2D();
         GraphicsContext graphicsContextMid = canvasMid.getGraphicsContext2D();
         GraphicsContext graphicsContextBottom = canvasBottom.getGraphicsContext2D();
-        new Thread(
+
+        /*new Thread(
                 () -> {
                     int frames = 0;
                     int fps = 200;
-                    int periodSecond = 2;
+                    int periodInSecond = 2;
                     int v1 = 0;
                     int interval = 1000 / fps;
-                    int pmToMovePerFrame = 200 / (fps * periodSecond / 2);
-                    int frameCelling = fps * periodSecond / 2;
+                    int pmToMovePerFrame = 200 / (fps * periodInSecond / 2);
+                    int frameCelling = fps * periodInSecond / 2;
                     int frameFloor = 0;
                     while (true) {
                         while (frames < frameCelling) {
@@ -94,45 +96,54 @@ public class MovingBallApplication extends Application {
                         }
                     }
                 }
-        ).start();
+        ).start();*/
+
         new Thread(
                 () -> {
                     int frames = 0;
-                    int fps = 512;
+                    int fps = 100;
                     int periodInSecond = 4; //s
                     //int periodInMillisecond = periodInSecond * 1000; //ms
                     int v = 0;
+                    int v1 = 0;
                     int interval = 1000 / fps;
                     int pmToMovePerFrame = 1024 / (fps * periodInSecond / 2) ;
+                    int pmToMovePerFrame1 = 200 / (fps * periodInSecond / 2);
                     int frameCelling = fps * periodInSecond / 2;
                     int frameFloor = 0;
                     while (true) {
                         while (frames < frameCelling) {
                             v += pmToMovePerFrame;
+                            v1 += pmToMovePerFrame1;
+                            graphicsContextTop.drawImage(imageWave, 0, v1);
                             graphicsContextMid.drawImage(image, v, positionY);
+                            graphicsContextBottom.drawImage(imageWave, 0, v1);
                             try {
                                 Thread.sleep(interval);
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             }
-                            //graphicsContextMid.clearRect(v, positionY, 1, 32);
+                            graphicsContextMid.clearRect(v, positionY, pmToMovePerFrame, 32);
                             frames++;
                         }
                         while (frames > frameFloor) {
                             v -= pmToMovePerFrame;
+                            v1 -= pmToMovePerFrame1;
+                            graphicsContextTop.drawImage(imageWave, 0, v1);
                             graphicsContextMid.drawImage(image, v, positionY);
+                            graphicsContextBottom.drawImage(imageWave, 0, v1);
                             try {
                                 Thread.sleep(interval);
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             }
-                            //graphicsContextMid.clearRect(v, positionY, 1, 32);
+                            graphicsContextMid.clearRect(v, positionY, pmToMovePerFrame, 32);
                             frames--;
                         }
                     }
                 }
         ).start();
-        new Thread(
+        /*new Thread(
                 () -> {
                     int frames = 0;
                     int fps = 200;
@@ -167,7 +178,7 @@ public class MovingBallApplication extends Application {
                         }
                     }
                 }
-        ).start();
+        ).start();*/
 
         stage.setOnCloseRequest(
                 windowEvent -> System.exit(0)
